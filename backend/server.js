f1c48189cf01a30152a130c1be465c92973b087d
Aman-garg-IITian <2020ucs0095@iitjammu.ac.in>
2023-10-23 22:44:10 +0530

    changed directory initialization

--- backend/server.js
+++ backend/server.js
@@ -5,37 +5,42 @@ import mongoose from "mongoose";
 import passportConfig from "./lib/passportConfig.js";
 import cors from "cors";
 import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 import authRoutes from "./routes/authRoutes.js";
 import downloadRoutes from "./routes/downloadRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import apiRoutes from "./routes/apiRoutes.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 mongoose.connect("mongodb://localhost:27017/jobPortal")
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.error(err));
 
 async function initializeDirectories() {
     try {
-        await fs.access("./public");
+        await fs.access(__dirname + "/public");
       } catch (error) {
         if (error.code === "ENOENT") {
-          await fs.mkdir("./public");
+          await fs.mkdir(__dirname + "/public");
         }
       }
     
       try {
-        await fs.access("./public/resume");
+        await fs.access(__dirname + "/public/resume");
       } catch (error) {
         if (error.code === "ENOENT") {
-          await fs.mkdir("./public/resume");
+          await fs.mkdir(__dirname + "/public/resume");
         }
       }
     
       try {
-        await fs.access("./public/profile");
+        await fs.access(__dirname +  "/public/profile");
       } catch (error) {
         if (error.code === "ENOENT") {
-          await fs.mkdir("./public/profile");
+          await fs.mkdir(__dirname + "/public/profile");
         }
       }
 }
